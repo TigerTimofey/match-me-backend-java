c3b235eeb9dab43ff7d8e0a07743818b316e3093
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2024-12-14 18:12:33 +0200

    token exprires time & Secret key

--- src/main/java/com/match/match/config/JwtAuthenticationFilter.java
+++ src/main/java/com/match/match/config/JwtAuthenticationFilter.java
@@ -16,12 +16,23 @@ import java.io.IOException;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private  final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(
             @NotNull HttpServletRequest request,
             @NotNull HttpServletResponse response,
             @NotNull FilterChain filterChain
     ) throws ServletException, IOException {
+final String authHeader = request.getHeader("Authorization");
+final String jwt;
+final String userEmail;
 
+if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+    filterChain.doFilter(request, response);
+    return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
     }
 }
